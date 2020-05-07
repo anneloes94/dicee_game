dfd7fa59da2265ff1e73306a89b3d84bee1e94ee
Anneloes <anneloes9405@hotmail.com>
2020-05-07 17:55:22 -0400

    changed refreshing into simple button
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const randomNumber1 = Math.ceil(Math.random() * 6);
-const randomNumber2 = Math.ceil(Math.random() * 6);
+function rollDice () {
+  const randomNumber1 = Math.ceil(Math.random() * 6);
+  const randomNumber2 = Math.ceil(Math.random() * 6);
+  
+  const diceOne = document.querySelector("img.img1")
+  const diceTwo = document.querySelector("img.img2")
+  
+  diceOne.setAttribute("src", `images/dice${randomNumber1}.png`)
+  diceTwo.setAttribute("src", `images/dice${randomNumber2}.png`)
+  
+  const playerOne = document.querySelectorAll("div.dice p")[0].innerHTML
+  const playerTwo = document.querySelectorAll("div.dice p")[1].innerHTML
+  
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = `${playerOne} wins!`
+  } else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = `${playerTwo} wins!`
+  } else {
+    document.querySelector("h1").innerHTML = "Draw!"
+  }
 
-const diceOne = document.querySelector("img.img1")
-const diceTwo = document.querySelector("img.img2")
-
-diceOne.setAttribute("src", `images/dice${randomNumber1}.png`)
-diceTwo.setAttribute("src", `images/dice${randomNumber2}.png`)
-
-const playerOne = document.querySelectorAll("div.dice p")[0].innerHTML
-const playerTwo = document.querySelectorAll("div.dice p")[1].innerHTML
-
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = `${playerOne} wins!`
-} else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = `${playerTwo} wins!`
-} else {
-  document.querySelector("h1").innerHTML = "Draw!"
 }
